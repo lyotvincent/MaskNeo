323ae98d3b64e3f80bd5757f61db84583561c049
lyrmagical <lyrmagical@gmail.com>
2025-07-13 09:35:28 +0000

    add new files

---
+++ compared/run_commands.sh
@@ -0,0 +1,5 @@
+# train BigMHC
+python src/train.py -i=src/train_data/train.csv -o=outputs -a=0 -p=1 -t=2
+
+# evaluate BigMHC, lastep means the last epoch
+python src/predict.py -i=src/test_data/test.csv -m=outputs/lastep -a=0 -p=1
